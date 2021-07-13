635f08dab44643ac19f9b9cd33b06a1342f0ead3
Daniel Rauch <aicypher@hotmail.com>
2021-07-14 01:18:50 +0300

    first upload, initial content of portfolio

---
+++ src/components/navigation.jsx
@@ -0,0 +1,68 @@
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>תפריט ניווט</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <a className='navbar-brand page-scroll' href='#page-top'>
+            דנה להצלחה
+          </a>{' '}
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#features' className='page-scroll'>
+                הנקודות הנכונות
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll'>
+                על עצמי
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+                מה אני מציעה
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                גלריה
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll'>
+                לקוחות ממליצים
+              </a>
+            </li>
+            <li>
+              <a href='#team' className='page-scroll'>
+                צוות
+              </a>
+            </li>
+            <li>
+              <a href='#contact' className='page-scroll'>
+                כל הצלחה מתחילה בשיחה ראשונה
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
---
+++ src/components/testimonials.jsx
@@ -0,0 +1,30 @@
+export const Testimonials = (props) => {
+  return (
+    <div id='testimonials'>
+      <div className='container'>
+        <div className='section-title text-center'>
+          <h2>מה הלקוחות אומרים</h2>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  <div className='testimonial'>
+                    <div className='testimonial-image'>
+                      {' '}
+                      <img src={d.img} alt='' />{' '}
+                    </div>
+                    <div className='testimonial-content'>
+                      <p>"{d.text}"</p>
+                      <div className='testimonial-meta'> - {d.name} </div>
+                    <div className='testimonial-meta-location'> - {d.location} </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
